11e107fc8a6d394cf907eb9a8f6c54ed8f0402a8
Mohd Faizan <faizan.mohd09@gmail.com>
2022-08-24 16:44:02 +0530

    nav

---
+++ src/Component/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from "../Component/HomePage.css"
+export default function HomePage() {
+  return (
+    <div className="body">
+      <div className='nav'>
+        <ul>
+          <li>Home</li>
+          <li>Latest</li>
+          <li>India</li>
+          <li>World</li>
+          <li>Delhi</li>
+          <li>Entertainment</li>
+          <li>Cricket</li>
+          <li>Lifestyle</li>
+          <li>Astrology</li>
+          <li>Editorial</li>
+          <li>For you</li>
+          <li>Shop Now</li>
+        </ul>
+      </div>
+      <div className="section">sfa</div>
+      <div className="footer">sfafs sfa</div>
+    </div>
+  );
+}
